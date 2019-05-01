0e37ed3ddb80f3949c51607249952f6ced304cc3
alee2 <alee2@bcchr.ca>
2019-05-01 13:48:51 -0700

    Version fixes.

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
-<?php 
+<?php
+require_once APP_PATH_DOCROOT . "ProjectGeneral/header.php"; 
 $customReportBuilder = new \BCCHR\CustomReportBuilder\CustomReportBuilder();
 $customReportBuilder->generateIndexPage();
+require_once APP_PATH_DOCROOT . "ProjectGeneral/footer.php";
