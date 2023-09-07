32e9213d3ddfa334623e31ed13736434450c8394
harneetbhinder99 <49669617+harneetbhinder99@users.noreply.github.com>
2023-09-07 11:16:51 +1000

    Update CustomTemplateEngine.php
    Fix to show records in the dropdown menu

--- CustomTemplateEngine.php
+++ CustomTemplateEngine.php
@@ -2555,8 +2555,8 @@ class CustomTemplateEngine extends \ExternalModules\AbstractExternalModule
         }
         ?>
         <!-- boostrap-select files -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.15/dist/css/bootstrap-select.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.15/dist/js/bootstrap-select.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/js/bootstrap-select.min.js"></script>
         <!-- js-cookie-->
         <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
         <!-- Module CSS -->
